969eb6c1627ee2d3c4e3dffa6fa3a6e1b04a4774
Yuki Komatsu <y.komatsu122@gmail.com>
2021-04-29 22:48:14 +0900

    feat: レコードの追加・更新・削除

--- flask_webapp/app/app.py
+++ flask_webapp/app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from models.models import OnegaiContent
+from models.database import session
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -17,6 +19,35 @@ def post():
     name = request.form["name"]
     all_onegai = OnegaiContent.query.all()
     return render_template("index.html", name=name, all_onegai=all_onegai)
+
+
+@app.route("/add", methods=["post"])
+def add():
+    title = request.form["title"]
+    body = request.form["body"]
+    content = OnegaiContent(title, body, datetime.now())
+    session.add(content)
+    session.commit()
+    return index()
+
+
+@app.route("/update", methods=["post"])
+def update():
+    content = OnegaiContent.query.filter_by(id=request.form["update"]).first()
+    content.title = request.form["title"]
+    content.body = request.form["body"]
+    session.commit()
+    return index()
+
+
+@app.route("/delete", methods=["post"])
+def delete():
+    id_list = request.form.getlist("delete")
+    for id in id_list:
+        content = OnegaiContent.query.filter_by(id=id).first()
+        session.delete(content)
+    session.commit()
+    return index()
 
 
 if __name__ == "__main__":
