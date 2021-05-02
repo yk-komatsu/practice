771a300d4f0f9b0b26c33a3ab07f6132efcc32af
Yuki Komatsu <y.komatsu122@gmail.com>
2021-05-02 22:43:25 +0900

    feat: ユーザー管理用テーブルの定義を追加

--- flask_webapp/models/models.py
+++ flask_webapp/models/models.py
@@ -17,3 +17,17 @@ class OnegaiContent(Base):
 
     def __repr__(self):
         return f"<Title {self.title}>"
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    user_name = Column(String(128), unique=True)
+    hashed_password = Column(String(128))
+
+    def __init__(self, user_name=None, hashed_password=None):
+        self.user_name = user_name
+        self.hashed_password = hashed_password
+
+    def __repr__(self):
+        return f"<Name {self.user_name}>"
