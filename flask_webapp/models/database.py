736250772ec70440056ac23e675407bb2db8dc95
Yuki Komatsu <y.komatsu122@gmail.com>
2021-05-02 23:00:36 +0900

    chore: DB接続情報を設定ファイルに記載するように変更

--- flask_webapp/models/database.py
+++ flask_webapp/models/database.py
@@ -1,12 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import configparser
+
+config_ini = configparser.ConfigParser()
+config_ini.read("models/config.ini", encoding="utf-8")
+
+db_user = config_ini["DATABASE"]["user"]
+db_password = config_ini["DATABASE"]["password"]
+db_ip = config_ini["DATABASE"]["ip"]
+db_name = config_ini["DATABASE"]["db_name"]
 
 DATABASE = "mysql://%s:%s@%s/%s?charset=utf8mb4" % (
-    "root",
-    "P@ssw0rd",
-    "127.0.0.1:3306",
-    "mydb",
+    db_user,
+    db_password,
+    db_ip,
+    db_name,
 )
 ENGINE = create_engine(DATABASE, convert_unicode=True, echo=True)
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=ENGINE))
