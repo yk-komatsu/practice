cbcbc8448f24ee4f13010c435db5e8d497a07f5d
Yuki Komatsu <y.komatsu122@gmail.com>
2021-05-03 22:21:31 +0900

    feat: ログイン処理の追加

--- flask_webapp/app/app.py
+++ flask_webapp/app/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
-from models.models import OnegaiContent
+from flask import Flask, render_template, request, session, redirect, url_for
+from models.models import OnegaiContent, User
 from models.database import session
 from datetime import datetime
+from hashlib import sha256
+from app import key
 
 app = Flask(__name__)
 
@@ -48,6 +50,24 @@ def delete():
         session.delete(content)
     session.commit()
     return index()
+
+
+@app.route("/login", methods=["post"])
+def login():
+    user_name = request.form["user_name"]
+    user = User.query.filter_by(user_name=user_name).first()
+    if user:
+        password = request.form["password"]
+        hashed_password = sha256(
+            (user_name + password + key.SALT).encode("utf-8")
+        ).hexdigest()
+        if user.hashed_password == hashed_password:
+            session["user_name"] = user_name
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("top", status="wrong_password"))
+    else:
+        return redirect(url_for("top", status="user_notfound"))
 
 
 if __name__ == "__main__":
